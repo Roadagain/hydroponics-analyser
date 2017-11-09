7c8b1dc2e299205a8d365b3f07a8852b7ddc8db3
Roadagain <ringoh72@gmail.com>
2017-11-09 17:18:16 +0900

    保存データからresultを削除

--- fetch.py
+++ fetch.py
@@ -10,6 +10,7 @@ def fetch_dict(url):
 
 def main():
     data = fetch_dict('http://localhost:5000/fetch/test')
+    del data['result']
     filename = dt.today().strftime('%Y%m%d%H%M%S') + '.json'
     f = open(filename, 'w')
     f.write(json.dumps(data))
