b95c0b662c47155444f694bb77dee158a0d9ff29
Roadagain <ringoh72@gmail.com>
2017-11-10 17:46:09 +0900

    pylintに従ってコード修正

--- server.py
+++ server.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 """The main program of hydroponics-analyser"""
 
-import fetch
 from datetime import datetime as dt
 import json
+import fetch
 
 def main():
     """Main function"""
     data = fetch.fetch_dict('http://localhost:5000/fetch/test')
     del data['result']
     filename = dt.today().strftime('%Y%m%d%H%M%S') + '.json'
-    f = open(filename, 'w')
-    f.write(json.dumps(data))
+    datafile = open(filename, 'w')
+    datafile.write(json.dumps(data))
 
 if __name__ == '__main__':
     main()
