7e47044f8165676c35faa94ec2a2d649b9f66d1f
Roadagain <ringoh72@gmail.com>
2017-11-10 17:32:24 +0900

    docstringを記述

--- fetch.py
+++ fetch.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+"""This module's functions fetch data from the api server"""
 
 import requests
 
 def fetch_dict(url):
+    """Fetches data and returns as a dict"""
     res = requests.get(url)
     return res.json()
--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+"""The main program of hydroponics-analyser"""
 
 import fetch
 from datetime import datetime as dt
 import json
 
 def main():
+    """Main function"""
     data = fetch.fetch_dict('http://localhost:5000/fetch/test')
     del data['result']
     filename = dt.today().strftime('%Y%m%d%H%M%S') + '.json'
